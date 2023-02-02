7ea321f4a8c9a2caeb9b501b7fa7566d19e4f955
Ivan <gallegosivan9@gmail.com>
2023-02-02 00:26:35 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/a20230201_ivangallegos_nycschools/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.a20230201_ivangallegos_nycschools;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.a20230201_ivangallegos_nycschools.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
